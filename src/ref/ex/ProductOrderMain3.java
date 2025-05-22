718bc0e4a14c89e0fb39de9b381ec4324a7efeb7
JisuEun <zkxktmxmfhvl@gmail.com>
2025-05-23 03:15:33 +0900

    ref.ex

--- src/ref/ex/ProductOrderMain3.java
+++ src/ref/ex/ProductOrderMain3.java
@@ -1,14 +1,31 @@
 package ref.ex;
 
-public class ProductOrderMain2 {
+import java.util.Scanner;
+
+public class ProductOrderMain3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("입력할 주문의 개수를 입력하세요: ");
+        int n = scanner.nextInt();
+
         // 여러 상품의 주문 정보를 담는 배열 생성
-        ProductOrder[] orders = new ProductOrder[3];
+        ProductOrder[] orders = new ProductOrder[n];
 
         // createOrder()를 여러번 사용해서 상품 주문 정보들을 생성하고 배열에 저장
-        orders[0] = createOrder("두부", 2000, 2);
-        orders[1] = createOrder("김치", 5000, 1);
-        orders[2] = createOrder("콜라", 1500, 2);
+        for (int i = 0; i < orders.length; i++) {
+            System.out.println((i+1) + "번째 주문 정보를 입력하세요.");
+            System.out.print("상품명: ");
+            String productName = scanner.nextLine();
+
+            System.out.print("가격: ");
+            int price = scanner.nextInt();
+
+            System.out.print("수량: ");
+            int quantity = scanner.nextInt();
+            scanner.nextLine(); // 입력 버퍼를 비우기 위한 코드
+
+            orders[i] = createOrder(productName, price, quantity);
+        }
 
         // printOrders()를 사용해서 상품 주문 정보 출력
         printOrders(orders);
